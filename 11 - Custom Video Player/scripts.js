12aa3f31c66279712ee6f26cf4c261e0511ea6bc
Loreto Delgado III <loretodelgadoiii@Loretos-MacBook-Pro.local>
2021-06-04 18:29:41 -0400

    added function that handles button skips

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -15,7 +15,17 @@ function updateButton(){
     toggle.textContent = icon;
 }
 
+function skip(){
+    video.currentTime += Number(this.dataset.skip)
+}
+
+function handleRangeUpdate(){
+    console.log(this.value);
+}
+
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
+skipButtons.forEach( button => button.addEventListener("click", skip))
+ranges.forEach( range => range.addEventListener("change", handleRangeUpdate))
