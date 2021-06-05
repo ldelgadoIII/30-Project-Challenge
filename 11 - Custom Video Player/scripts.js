59af6b6ad8bcc2d1cff2f13894db27a586e26a29
Loreto Delgado III <loretodelgadoiii@Loretos-MacBook-Pro.local>
2021-06-05 17:18:56 -0400

    added functionality that updates progress bar as user scrubs through video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,6 +5,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+let mousedown = false;
 
 function togglePlay(){
     video.paused ? video.play() : video.pause();
@@ -20,12 +21,35 @@ function skip(){
 }
 
 function handleRangeUpdate(){
-    console.log(this.value);
+    video[this.name] = this.value;
+}
+
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+function scrubProgress(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * 100;
+    if(mousedown){
+        progressBar.style.flexBasis = `${scrubTime}%`
+    }
 }
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+progress.addEventListener("click", scrub)
+progress.addEventListener("mousemove", () => mousedown && scrub)
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
+progress.addEventListener("mousemove", scrubProgress)
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach( button => button.addEventListener("click", skip))
 ranges.forEach( range => range.addEventListener("change", handleRangeUpdate))
